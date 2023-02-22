22580a372d31684b82ddf5079cc6fa5668630f3d
lukeheo8592 <71468858+lukeheo8592@users.noreply.github.com>
2023-02-22 14:02:34 -0500

    easy 3

--- 1~10.js
+++ 1~10.js
@@ -48,3 +48,138 @@ if(Number.isInteger(x.toString().length/2)){
     }
     
 };
+
+
+// Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+// Symbol       Value
+// I             1
+// V             5
+// X             10
+// L             50
+// C             100
+// D             500
+// M             1000
+// For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+// Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+// I can be placed before V (5) and X (10) to make 4 and 9. 
+// X can be placed before L (50) and C (100) to make 40 and 90. 
+// C can be placed before D (500) and M (1000) to make 400 and 900.
+// Given a roman numeral, convert it to an integer.
+
+
+var romanToInt = function(s) {
+    let tnd;
+    let hnd;
+    let ten;
+    let one;
+  let count = 0;
+  let order = 0;
+        for(let i = 0; i < s.length; i++){
+            if(s[i] == "M"){
+                count++;
+                order++;
+            }else if(s[i] == "C"){
+                break;
+            }else{
+              break;
+            }
+        }
+        
+   tnd = 1000*count;
+   count = 0;
+        
+            if(s[order] == "C"){
+                order++;
+                count++;
+                if(s[order] == "M"){
+                    order++;
+                    count = 9;
+                }else if(s[order] == "D"){
+                     order++;
+                    count = 4;
+                }else if(s[order] == "C"){
+                    order++;
+                    count++;
+                    if(s[order] == "C"){
+                         order++;
+                         count++;
+                    }
+
+                }
+            
+            }else if(s[order] == "D"){
+                order++;
+                count = 5;
+                while(s[order] == "C"){
+                         order++;
+                         count++;
+                }
+            }
+            hnd = count * 100;
+            count = 0;
+
+            if(s[order] == "X"){
+                order++;
+                count++;
+                if(s[order] == "C"){
+                    order++;
+                    count = 9;
+                }else if(s[order] == "L"){
+                     order++;
+                    count = 4;
+                }else if(s[order] == "X"){
+                    order++;
+                    count++;
+                    if(s[order] == "X"){
+                         order++;
+                         count++;
+                    }
+
+                }
+            
+            }else if(s[order] == "L"){
+                order++;
+                count = 5;
+                while(s[order] == "X"){
+                         order++;
+                         count++;
+                }
+            }
+            ten = count * 10;
+            count = 0;
+
+            if(s[order] == "I"){
+                order++;
+                count++;
+                if(s[order] == "X"){
+                    order++;
+                    count = 9;
+                }else if(s[order] == "V"){
+                     order++;
+                    count = 4;
+                }else if(s[order] == "I"){
+                    order++;
+                    count++;
+                    if(s[order] == "I"){
+                         order++;
+                         count++;
+                    }
+
+                }
+            
+            }else if(s[order] == "V"){
+                order++;
+                count = 5;
+                while(s[order] == "I"){
+                         order++;
+                         count++;
+                }
+            }
+            one = count;
+            let result = one + ten + hnd+ tnd;
+            return result;
+
+};
