bd276f88001019dec5253ee294e1e215b7b6bde3
lukeheo8592 <71468858+lukeheo8592@users.noreply.github.com>
2023-02-24 00:09:20 -0500

    easy no.4

--- 1~10.js
+++ 1~10.js
@@ -227,3 +227,31 @@ var longestCommonPrefix = function(strs) {
       return prefix;
     }
   };
+
+
+//   Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+// An input string is valid if:
+
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+// Every close bracket has a corresponding open bracket of the same type.
+
+
+
+var isValid = function(s) {
+    if (s.length <=1) return false
+    
+    let stack = []
+    let hash = {
+        '(' : ')',
+        '[' : ']',
+        '{' : '}'
+    }
+    
+    for(let i = 0; i < s.length; i++){
+        if (hash[s[i]]) stack.push(hash[s[i]])
+        else if (s[i] !== stack.pop()) return false
+    }
+    return !stack.length
+};
