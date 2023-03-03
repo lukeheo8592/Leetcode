3aa054ea738d2a6f744c1bce7964b5e53c4ec381
lukeheo8592 <71468858+lukeheo8592@users.noreply.github.com>
2023-03-02 22:59:29 -0500

    baekjon no.2

--- baekjoon.js
+++ baekjoon.js
@@ -1,3 +1,12 @@
 //Hello World!를 출력하시오.
 
 console.log("Hello World!")
+
+
+// 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+var fs = require('fs');
+var input = fs.readFileSync('/dev/stdin').toString().split(' ');
+var a = parseInt(input[0]);
+var b = parseInt(input[1]);
+console.log(a+b);
